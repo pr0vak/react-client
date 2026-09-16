6567439d8f621a7b927a0595059af2a8104ad5f0
Vyacheslav <slava93omsk@mail.ru>
2026-09-16 08:16:30 +0600

    006 Bootstrap и react-bootstrap: подключение стилей и первая <Button>.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Button, Container } from 'react-bootstrap';
+
 function App() {
   return (
-		<></>
-	)
+		<Container className='py-4'>
+			<h1>Product API Client</h1>
+
+			<Button variant='primary'>
+				Hello world
+			</Button>
+		</Container>
+	);
 }
 
 export default App
